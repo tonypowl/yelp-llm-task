95c1e2d9706100fc034d1ade478578849591f804
tonypowl <tonypaul0412@gmail.com>
2025-12-15 23:35:16 +0530

    basic working code, to work on the html script

--- Task2/app.py
+++ Task2/app.py
@@ -1,19 +1,20 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
 import google.generativeai as genai
 import json, os
 from datetime import datetime
 
-# Gemini setup
+#loading env variables
+load_dotenv()
+
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("models/gemini-flash-latest")
-
 app = FastAPI()
 templates = Jinja2Templates(directory="profile")
 
 DATA_FILE = "data.json"
-
 
 def load_data():
     if not os.path.exists(DATA_FILE):
@@ -21,38 +22,44 @@ def load_data():
     with open(DATA_FILE, "r") as f:
         return json.load(f)
 
-
 def save_data(entry):
     data = load_data()
     data.append(entry)
     with open(DATA_FILE, "w") as f:
         json.dump(data, f, indent=2)
 
+def gemini_call(prompt, fallback):
+    try:
+        return model.generate_content(prompt).text
+    except Exception:
+        return fallback
 
-# ---------------- USER DASHBOARD ----------------
-
+#user dashboard
 @app.get("/", response_class=HTMLResponse)
 def user_dashboard(request: Request):
     return templates.TemplateResponse("user.html", {"request": request})
 
-
+#submit button (post request)
 @app.post("/submit", response_class=HTMLResponse)
 def submit_review(
     request: Request,
     rating: int = Form(...),
     review: str = Form(...)
 ):
-    ai_response = model.generate_content(
-        f"Write a friendly response to this customer review:\n{review}"
-    ).text
+    ai_response = gemini_call(
+        f"write a customer friendly response to this review:\n{review}",
+        "Thanks for your feedback! kinda message nothing too long"
+    )
 
-    summary = model.generate_content(
-        f"Summarize this review in one short sentence:\n{review}"
-    ).text
+    summary = gemini_call(
+        f"summarize this review in one short sentence:\n{review}",
+        "ex : customer shared positive feedback about their experience."
+    )
 
-    action = model.generate_content(
-        f"Suggest one clear action a business should take based on this review:\n{review}"
-    ).text
+    action = gemini_call(
+        f"suggest one clear action that should be taken based on the review:\n{review}",
+        "Review feedback and look for areas of improvement."
+    )
 
     save_data({
         "rating": rating,
@@ -71,9 +78,7 @@ def submit_review(
         }
     )
 
-
-# ---------------- ADMIN DASHBOARD ----------------
-
+#admin dashboard
 @app.get("/admin", response_class=HTMLResponse)
 def admin_dashboard(request: Request):
     data = load_data()
